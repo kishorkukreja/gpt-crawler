22643cbdc3269fc18b75ba9b726ddb89f8239d59
Kishor Kukreja <kishorkukreja76@gmail.com>
2024-01-07 22:16:55 +0000

    Update config.ts and config.ts

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  url: "https://www.zenflowchart.com/blog",
+  match: "https://www.zenflowchart.com/blog/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
   maxTokens: 2000000,
--- containerapp/data/config.ts
+++ containerapp/data/config.ts
@@ -1,8 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  url: "https://www.zenflowchart.com/blog",
+  match: "https://www.zenflowchart.com/blog/**",
   maxPagesToCrawl: 50,
   outputFileName: "../data/output.json",
+  maxTokens: 2000000,
 };
